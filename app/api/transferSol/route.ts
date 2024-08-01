e300d4799dfede54460e4b839736998a3b8f9564
Parth Parmar <softsparths@gmail.com>
2024-08-01 09:17:09 +0530

    chore: Update npm dependencies and add Solana Actions package

--- app/api/transferSol/route.ts
+++ app/api/transferSol/route.ts
@@ -1,29 +1,92 @@
-import bs58 from "bs58";
-import { PublicKey, Keypair, Connection, Transaction, SystemProgram, sendAndConfirmTransaction, LAMPORTS_PER_SOL } from "@solana/web3.js";
+import {
+    ACTIONS_CORS_HEADERS,
+    ActionGetResponse,
+    ActionPostRequest,
+    ActionPostResponse,
+    createPostResponse,
+} from "@solana/actions";
 
-const connection = new Connection("https://api.devnet.solana.com", "confirmed");
+import {
+    Connection,
+    LAMPORTS_PER_SOL,
+    PublicKey,
+    SystemProgram,
+    Transaction,
+    clusterApiUrl,
+} from "@solana/web3.js";
 
-export async function transferSol(request: Request) {
+export async function GET(request: Request) {
+    const url = new URL(request.url);
+    const payload: ActionGetResponse = {
+        icon: "https://pbs.twimg.com/profile_images/1809632821553819651/-ZNEJXp0_400x400.jpg", // Icon URL
+        title: "Transfer to the CodeParth",
+        description: "Support CodeParth by transferring SOL.",
+        label: "Transfer SOL to CodeParth",
+        links: {
+            actions: [
+                {
+                    label: "Transfer 0.01 SOL",
+                    href: `${url.href}?amount=0.01`,
+                },
+            ],
+        },
+    };
+    return Response.json(payload, {
+        headers: ACTIONS_CORS_HEADERS,
+    });
+}
+
+export const OPTIONS = GET;
+
+export async function POST(request: Request) {
+    const body: ActionPostRequest = await request.json();
+
+    const url = new URL(request.url);
+
+    const amount = Number(url.searchParams.get("amount")) || 0.01;
+
+    let sender;
+
     try {
-        const { sender, receiver, solAmmount } = await request.json();
-
-        if (!sender || !receiver || !solAmmount) {
-            return new Response(JSON.stringify({ error: "Missing required fields" }), { status: 400, headers: { "content-type": "application/json" } });
-        }
-
-        const transaction = new Transaction().add(
-            SystemProgram.transfer({
-                fromPubkey: Keypair.fromSecretKey(bs58.decode(sender)).publicKey,
-                toPubkey: new PublicKey(receiver),
-                lamports: LAMPORTS_PER_SOL * solAmmount,
-            })
-        );
-
-        const signature = await sendAndConfirmTransaction(connection, transaction, [Keypair.fromSecretKey(bs58.decode(sender))]);
-
-        return new Response(JSON.stringify({ signature }), { headers: { "content-type": "application/json" } });
+        sender = new PublicKey(body.account);
     } catch (error) {
-        console.error("Error processing transaction:", error);
-        return new Response(JSON.stringify({ error: "Internal Server Error" }), { status: 500, headers: { "content-type": "application/json" } });
+        return Response.json(
+            {
+                error: {
+                    message: "Invalid account",
+                },
+            },
+            {
+                status: 400,
+                headers: ACTIONS_CORS_HEADERS,
+            }
+        );
     }
+
+    const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
+
+    const transaction = new Transaction().add(
+        SystemProgram.transfer({
+            fromPubkey: sender,
+            toPubkey: new PublicKey("ETVZ97k3rZv96cwp3CYpPoBC74PKkQsNQ4ex6NHx2hRx"),
+            lamports: amount * LAMPORTS_PER_SOL,
+        })
+    );
+    transaction.feePayer = sender;
+    transaction.recentBlockhash = (
+        await connection.getLatestBlockhash()
+    ).blockhash;
+    transaction.lastValidBlockHeight = (
+        await connection.getLatestBlockhash()
+    ).lastValidBlockHeight;
+
+    const payload: ActionPostResponse = await createPostResponse({
+        fields: {
+            transaction,
+            message: "Transaction created",
+        },
+    });
+    return new Response(JSON.stringify(payload), {
+        headers: ACTIONS_CORS_HEADERS,
+    });
 }
