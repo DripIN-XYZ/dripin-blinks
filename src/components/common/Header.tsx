f7136d585bbd22ac7f158d5c7665a4de30062816
Parth Parmar <softsparths@gmail.com>
2024-09-02 03:11:48 +0530

    refactor: Update Header component to include Create Blink button

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -16,13 +16,12 @@ export default function Header() {
                 />
                 <h1 className="text-2xl text-blue-600 font-Andvari font-normal">Dripin</h1>
             </Link>
-            {/* <Link href="/create">
-                <ConnectWallet />
+            <Link href="/create">
                 <Button className="bg-blue-600 hover:bg-blue-500 focus-visible:ring-blue-800 text-base font-Andvari">
                     Create Blink
                 </Button>
-            </Link> */}
-            <ConnectWallet />
+            </Link>
+            {/* <ConnectWallet /> */}
         </header>
     );
 }
