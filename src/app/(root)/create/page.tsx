2eedd499098e8302cc9a6079e3a3564d23b3d671
Parth Parmar <softsparths@gmail.com>
2024-09-03 03:01:22 +0530

    refactor: form progress to step 3

--- src/app/(root)/create/page.tsx
+++ src/app/(root)/create/page.tsx
@@ -1,16 +1,29 @@
 "use client";
 
 import Header from "./_components/Header";
-import { useEffect, useState } from "react";
+import fetchTokens from "@/lib/searchAssets";
+import NextImage from "@/components/NextImage";
 import ConnectWallet from "@/components/wallet";
+import { Button } from "@/components/ui/button";
 import Wrapper from "@/components/common/Wrapper";
+import { useEffect, useState, Suspense } from "react";
 import { useWallet } from "@solana/wallet-adapter-react";
+import { ScrollArea } from "@/components/ui/scroll-area";
 import FormPagination from "@/components/createBlink/formPagination";
+import { Grouping, Item, ItemsResponse } from "@/types/SearchAssetsType";
 
 export default function CreateBlink() {
     const { publicKey, disconnecting } = useWallet();
-    const [currentFormPage, setCurrentFormPage] = useState(1);
+    const [currentFormPage, setCurrentFormPage] = useState<number>(1);
     const formPage = Array.from({ length: 7 }, (_, i) => i + 1);
+
+    const [tokens, setTokens] = useState<ItemsResponse | null>(null);
+    const [collectionDetails, setCollectionDetails] = useState<Grouping[]>([]);
+
+    const [selectedCollectionAddress, setSelectedCollectionAddress] = useState<string>("");
+    const [specificCollectionDetails, setSpecificCollectionDetails] = useState<Item[] | null>(null);
+
+    const [selectedNFTmintAddress, setSelectedNFTmintAddress] = useState<string>("");
 
     useEffect(() => {
         if (publicKey) {
@@ -20,6 +33,44 @@ export default function CreateBlink() {
             setCurrentFormPage(1);
         }
     }, [publicKey, disconnecting]);
+
+    useEffect(() => {
+        if (publicKey) {
+            const walletAddress = publicKey.toString();
+            fetchTokens(walletAddress).then((data) => {
+                setTokens((data as unknown) as ItemsResponse);
+            }).catch(console.error);
+        }
+        else {
+            setTokens(null);
+        }
+    }, [publicKey])
+
+    useEffect(() => {
+        const collectionMap = new Map<string, Grouping>();
+        if (!tokens) {
+            return;
+        }
+        tokens?.items.items.forEach((nft) => {
+            const collection = nft.grouping.find((g) => g.group_key === "collection");
+            if (collection && collection.collection_metadata) {
+                collectionMap.set(collection.group_value, collection);
+            }
+        });
+        setCollectionDetails(Array.from(collectionMap.values()));
+    }, [tokens]);
+
+    useEffect(() => {
+        if (selectedCollectionAddress) {
+            if (tokens) {
+                const collectionNFTData = tokens.items.items.filter((nft) => {
+                    const collection = nft.grouping.find((g) => g.group_key === "collection");
+                    return collection && collection.group_value === selectedCollectionAddress;
+                });
+                setSpecificCollectionDetails((collectionNFTData as unknown) as Item[]);
+            }
+        }
+    }, [selectedCollectionAddress, tokens]);
 
     return (
         <Wrapper
@@ -44,11 +95,71 @@ export default function CreateBlink() {
                             <div className="h-full flex flex-col justify-center">
                                 <h1 className="text-5xl font-bold">Select a Collection</h1>
                                 <h2 className="pt-2 text-xl font-normal text-black">Choose the collection you want to list an NFT from.</h2>
+                                <ScrollArea className="pt-4 w-full h-[70vh] overflow-hidden">
+                                    <div className="grid grid-cols-3 max-lg:grid-cols-2 gap-4">
+                                        {collectionDetails && collectionDetails.map((collection, index) => (
+                                            <Button
+                                                key={index}
+                                                onClick={() => {
+                                                    setSelectedCollectionAddress(collection.group_value);
+                                                    setCurrentFormPage(currentFormPage + 1)
+                                                }}
+                                                variant="secondary"
+                                                className="h-fit border-2 border-blue-600 bg-blue-100 hover:bg-blue-200 focus-visible:ring-blue-800 text-sm"
+                                            >
+                                                <Suspense fallback={<div />}>
+                                                    <div className="flex flex-col w-full h-fit gap-2 pt-2">
+                                                        <NextImage
+                                                            src={collection.collection_metadata.image}
+                                                            alt={collection.collection_metadata.name}
+                                                            width={192}
+                                                            height={192}
+                                                            className="aspect-square object-contain w-full h-full rounded-sm border-blue-600 border-2"
+                                                        />
+                                                        <p className="text-sm">{collection.collection_metadata.name}</p>
+                                                    </div>
+                                                </Suspense>
+                                            </Button>
+                                        ))}
+                                    </div>
+                                </ScrollArea>
                             </div>
                         ) : currentFormPage === 3 ? (
                             <div className="h-full flex flex-col justify-center">
                                 <h1 className="text-5xl font-bold">Select Your NFT!</h1>
-                                <h2 className="pt-2 text-xl font-normal text-black">Choose the NFT you want to list from your collection.</h2>
+                                <h2 className="pt-2 text-xl font-normal text-black">{`Choose the NFT you want to list from `}
+                                    <strong>
+                                        {specificCollectionDetails && specificCollectionDetails[0].grouping[0].collection_metadata.name}.
+                                    </strong>
+                                </h2>
+                                <ScrollArea className="pt-4 w-full h-[70vh] overflow-hidden">
+                                    <div className="grid grid-cols-3 max-lg:grid-cols-2 gap-4">
+                                        {specificCollectionDetails && specificCollectionDetails.map((nft, index) => (
+                                            <Button
+                                                key={index}
+                                                onClick={() => {
+                                                    setSelectedNFTmintAddress(nft.id);
+                                                    setCurrentFormPage(currentFormPage + 1)
+                                                }}
+                                                variant="secondary"
+                                                className="h-fit border-2 border-blue-600 bg-blue-100 hover:bg-blue-200 focus-visible:ring-blue-800 text-sm"
+                                            >
+                                                <Suspense fallback={<div />}>
+                                                    <div className="flex flex-col w-full h-fit gap-2 pt-2">
+                                                        <NextImage
+                                                            src={nft.content.links.image}
+                                                            alt={nft.content.metadata.name}
+                                                            width={192}
+                                                            height={192}
+                                                            className="aspect-square object-contain w-full h-full rounded-sm border-blue-600 border-2"
+                                                        />
+                                                        <p className="text-sm">{nft.content.metadata.name}</p>
+                                                    </div>
+                                                </Suspense>
+                                            </Button>
+                                        ))}
+                                    </div>
+                                </ScrollArea>
                             </div>
                         ) : currentFormPage === 4 ? (
                             <div className="h-full flex flex-col justify-center">
---
+++ src/lib/searchAssets.ts
@@ -0,0 +1,44 @@
+// import { ItemsResponse } from "@/types/SearchAssetsType";
+
+
+interface Tokens {
+    items: any[];
+}
+
+const fetchTokens = async (walletAddress: string): Promise<Tokens> => {
+    const url = `https://mainnet.helius-rpc.com/?api-key=${process.env.NEXT_PUBLIC_HELIUS_API_KEY}` || "https://mainnet.helius-rpc.com";
+    console.log(
+        `Starting search for tokens for wallet address: ${walletAddress}`
+    );
+    try {
+        const response = await fetch(url, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                jsonrpc: "2.0",
+                id: "my-id",
+                method: "searchAssets",
+                params: {
+                    ownerAddress: walletAddress,
+                    tokenType: "all",
+                    displayOptions: {
+                        showCollectionMetadata: true,
+                    },
+                },
+            }),
+        });
+        const data = await response.json();
+        console.log(
+            `Data returned for wallet address ${walletAddress}:`,
+            data.result
+        );
+        return { items: data.result };
+    } catch (error) {
+        console.error("Error fetching tokens:", error);
+        return { items: [] };
+    }
+};
+
+export default fetchTokens;
