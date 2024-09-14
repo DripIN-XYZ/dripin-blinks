2e7fe2cc1c4baf70e49e9cdbfce0ab061c6beda9
Parth Parmar <softsparths@gmail.com>
2024-09-15 02:27:10 +0530

    feat: Add POST endpoint for NFT purchase functionality in buyNFT route

--- src/app/api/buyNFT/[...buyNFT]/route.ts
+++ src/app/api/buyNFT/[...buyNFT]/route.ts
@@ -8,19 +8,16 @@ import {
 
 import {
     Connection,
-    LAMPORTS_PER_SOL,
     PublicKey,
-    SystemProgram,
     Transaction,
-    clusterApiUrl,
 } from "@solana/web3.js";
 import axios from "axios";
 
+const marketplace_address = "3mycE4xdRESBX8pchRTS2UfBChQnRgCU3GKvGTeZxLVH";
 
 export async function GET(request: Request) {
     const url = new URL(request.url);
     const listAddress = request.url.split("/").pop();
-    const marketplace_address = "3mycE4xdRESBX8pchRTS2UfBChQnRgCU3GKvGTeZxLVH"
 
     if (!listAddress) {
         return new Response("Invalid list address", { status: 400 });
@@ -56,3 +53,72 @@ export async function GET(request: Request) {
         return new Response("Error fetching NFT metadata", { status: 500 });
     }
 };
+
+export async function POST(request: Request) {
+    const url = new URL(request.url);
+    const listAddress = request.url.split("/").pop();
+    const body: ActionPostRequest = await request.json();
+
+    let buyerAddress;
+
+    try {
+        buyerAddress = new PublicKey(body.account);
+
+        const nftData = await axios.get(`https://api.shyft.to/sol/v1/marketplace/list_details?network=mainnet-beta&marketplace_address=${marketplace_address}&list_state=${listAddress}`, {
+            headers: {
+                'x-api-key': process.env.NEXT_PUBLIC_SHYFT_API_KEY
+            }
+        });
+
+        const response = await axios.post("https://api.shyft.to/sol/v1/marketplace/list",
+            {
+                network: "mainnet-beta",
+                nft_address: nftData.data.result.list_state,
+                marketplace_address: nftData.data.result.marketplace_address,
+                price: nftData.data.result.price,
+                seller_address: nftData.data.result.seller_wallet,
+                buyer_wallet: buyerAddress,
+            },
+            {
+                headers: {
+                    'x-api-key': process.env.NEXT_PUBLIC_SHYFT_API_KEY,
+                    'Content-Type': 'application/json'
+                }
+            });
+
+        const encTx = response.data.result.encoded_transaction;
+
+        const connection = new Connection('https://api.mainnet-beta.solana.com');
+        const encodedTransaction = encTx;
+        const transaction = Transaction.from(Buffer.from(encodedTransaction, 'base64'));
+
+        const signature = await connection.sendRawTransaction(transaction.serialize(), {
+            skipPreflight: true,
+        });
+
+        await connection.confirmTransaction(signature, 'singleGossip');
+
+        const payload: ActionPostResponse = await createPostResponse({
+            fields: {
+                transaction,
+                message: "Transaction created",
+            },
+        });
+        return new Response(JSON.stringify(payload), {
+            headers: ACTIONS_CORS_HEADERS,
+        });
+    } catch (error) {
+        return Response.json(
+            {
+                error: {
+                    message: "Invalid account",
+                },
+            },
+            {
+                status: 400,
+                headers: ACTIONS_CORS_HEADERS,
+            }
+        );
+    }
+
+}
