f44372ce1df83d4818727b0db2fe11da2fe62e5a
Parth Parmar <softsparths@gmail.com>
2024-10-06 13:55:26 +0530

    feat: update commission

--- src/app/api/buyNFT/[...buyNFT]/route.ts
+++ src/app/api/buyNFT/[...buyNFT]/route.ts
@@ -93,7 +93,7 @@ export interface getNFTBuyType {
 const FEE_RECIPIENT_ADDRESS = "ETVZ97k3rZv96cwp3CYpPoBC74PKkQsNQ4ex6NHx2hRx";
 
 const calculateFee = (maxPrice: number): number => {
-    return Math.floor(maxPrice * 0.015);
+    return Math.floor(maxPrice * 0.055);
 };
 
 const getListAddress = (url: string): string => {
